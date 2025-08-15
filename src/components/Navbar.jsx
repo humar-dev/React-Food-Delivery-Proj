77b94b0d6615ccf9d5025725c29c9755f7e1ae94
umar farooq <umarfarooq.workk@gmail.com>
2025-08-15 23:09:24 +0500

    Refine navbar styling and layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../assets/assets'
 
 function Navbar() {
   return (
     <div className='app-navbar'>
-      
+       <img src= {assets.logo} className='logo'></img>
+        <ul className="navbar-menu">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Mobile-App</li>
+          <li>Contact Us</li>
+        </ul>
+
+        <div className='navbar-right'>
+           <img src={assets.search_icon}/>
+            <div className="navbar-search-div">
+              <img src={assets.basket_icon}/>
+               <div className="dot"></div>
+            </div>
+             <button>Sign In</button>
+        </div>
     </div>
   )
 }
