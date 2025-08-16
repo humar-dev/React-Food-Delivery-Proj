f1d99dfd3fe8e3e5fe475ffc58cc9a80622000a1
umar farooq <umarfarooq.workk@gmail.com>
2025-08-16 23:43:50 +0500

    Implement header section with background image and intro text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
-// import React from 'react'
-// import './Header.css'
+import React from 'react'
+import './Header.css'
 
-// function Header() {
-//   return (
-//     <div className='header'>
-//       <div className="header-contents">
-//           <h2>Order you favourite Food</h2>
-//           <p>Choose from a diverse menu featuring a  detectable array of dishes crafted with the finest ingredients  and culinary satisfied your cravings and elevate your dining experience , one meal at a time </p>
-//           <button>Menu Items</button>
-//       </div>
+function Header() {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+          <h2>Order you favourite Food</h2>
+          <p>Choose from a diverse menu featuring a  detectable array of dishes crafted with the finest ingredients  and culinary satisfied your cravings and elevate your dining experience , one meal at a time </p>
+          <button>Menu Items</button>
+      </div>
 
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default Header
+export default Header
