d7528f29974011375e74feefdbda2d8799820e8c
umar farooq <umarfarooq.workk@gmail.com>
2025-08-17 22:05:29 +0500

    Fix issue of Menu-Items

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -9,10 +9,13 @@ function ExploreMenu() {
             <p className='explore-menu-text'>Choose from a diverse menu featuring a  detectable array of dishes crafted with the finest ingredients</p>
             <div className="explore-menu-list">
                 {menu_list.map((item, index) => {
+
+
+
                     return (
                         <div key={index} className='explore-menu-list-item'>
-                            <img src={item.menu_image}></img>
-                            <p>{item.menu_name}</p>
+                            {/* <img src={item.menu_image}></img>
+                            <p>{item.menu_name}</p> */}
 
                         </div>
                     )
