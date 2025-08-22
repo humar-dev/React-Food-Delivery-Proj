39af246b1ff1df54eb2e5de9e06afea3c21009e3
umar farooq <umarfarooq.workk@gmail.com>
2025-08-22 23:14:00 +0500

    add item count functionality for food items

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
 import './FoodItem.css'
 
-function FoodItem({id , name , price , description , image }) {
-       
+function FoodItem({ id, name, price, description, image }) {
+
+  const [CountItem, SetCountItem] = useState(0)
+
   return (
-    <div  className='food-item'>
-       <div className="food-item-img-container">
-           <img className='food-item-image' src={image} alt='food-item-image'/>
-       </div>
-
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                 <img src={assets.rating_starts}></img>
+    <div className='food-item'>
+      <div className="food-item-img-container">
+        <img className='food-item-image' src={image} alt='food-item-image' />
+        {
+          !CountItem
+            ? <img className='add' onClick={() => SetCountItem(prev => prev + 1)} src={assets.add_icon_white} />
+            : <div className='food-item-counter'>
+               <img  onClick={()=>SetCountItem(prev=>prev-1)} src={assets.remove_icon_red} />
+               <p>{CountItem}</p>
+               <img onClick={()=>SetCountItem(prev=>prev+1)} src={assets.add_icon_green}/>
             </div>
-            <div className="food-item-desc">{description}</div>
-            <div className="food-item-price">${price}</div>
 
+        }
+
+
+      </div>
+
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts}></img>
         </div>
+        <div className="food-item-desc">{description}</div>
+        <div className="food-item-price">${price}</div>
+
+      </div>
 
     </div>
   )
